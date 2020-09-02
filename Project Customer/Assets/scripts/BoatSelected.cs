7980272ea9ec69ae3dbd91d94d611495ed23b9b9
Pepijn Wasser <482906@student.saxion.nl>
2020-09-02 14:42:08 +0200

    basic pathfinding and camera

---
+++ Project Customer/Assets/scripts/BoatSelected.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSelected : MonoBehaviour
+{
+    public bool selected = false;
+    public Material selectedMaterial;
+    public Material notSelectedMaterial;
+
+    MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = notSelectedMaterial;
+    }
+
+    void Update()
+    {
+        //Debug.Log(selected);
+        if(selected == true)
+        {
+            meshRenderer.material = selectedMaterial;
+        }
+        else
+        {
+            meshRenderer.material = notSelectedMaterial;
+        }
+    }
+}
---
+++ Project Customer/Assets/scripts/emptyScrip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class emptyScrip : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotationSpeed;
+
+    private void Update()
+    {
+        Controls();
+        Rotate();
+    }
+
+
+    void Controls()
+    {
+        Vector3 moveVec = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+        transform.Translate(moveVec * moveSpeed * Time.deltaTime);
+     
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+    }
+}
