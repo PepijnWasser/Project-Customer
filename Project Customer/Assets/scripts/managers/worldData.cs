697adce0eeadc2815ce1649613dd6cc00d193b0c
Pepijn Wasser <482906@student.saxion.nl>
2020-09-04 17:57:37 +0200

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/PepijnWasser/Project-Customer"""
    This reverts commit 487f91186b1c1768fcbacd225caf65c4a645f6fc.

---
+++ Project Customer/Assets/scripts/managers/worldData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldData : MonoBehaviour
+{
+    public int woodVolume;
+    public int plasticVolume;
+    public int oilVolume;
+}
