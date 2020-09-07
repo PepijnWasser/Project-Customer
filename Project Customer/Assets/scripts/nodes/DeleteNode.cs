35a78e438ff5d9af91eed7a0dd30b849c455c7f6
Pepijn Wasser <482906@student.saxion.nl>
2020-09-07 14:39:36 +0200

    upload pepijn

---
+++ Project Customer/Assets/scripts/nodes/DeleteNode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteNode : MonoBehaviour
+{
+    bool selected;
+    public Transform objectCreatedFor;
+    
+    void Update()
+    {
+        selected = GetComponent<Selected>().selected;
+        
+        if(selected == true)
+        {
+            if (Input.GetKey(KeyCode.Delete) || Input.GetKey(KeyCode.Backspace))
+            {               
+                objectCreatedFor.GetComponent<MovementQue>().RemoveCoordinateFromQue(transform.position);
+                GameObject selectionManager = GameObject.FindGameObjectWithTag("selectionManager");
+                selectionManager.GetComponent<selectObjects>().selectedObjects.Remove(gameObject);
+            }
+        }        
+    }
+
+    
+}
