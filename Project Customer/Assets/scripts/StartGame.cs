0bc4fce9133369396a9b12dab21211b253f9b1a9
Pepijn Wasser <482906@student.saxion.nl>
2020-09-08 15:32:09 +0200

    update from master

---
+++ Project Customer/Assets/scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void StartSpel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+}
+
---
+++ Project Customer/Assets/scripts/resources/wareHouse.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WareHouse : MonoBehaviour
+{
+    private List<GameObject> boats = new List<GameObject>();
+    public int wareHousePickupRange;
+
+    private int plasticVolume;
+    private int oilVolume;
+    private int woodVolume;
+
+    public int wareHouseInventorySpace;
+    public int wareHouseMaxPlastic;
+    public int wareHouseMaxWood;
+    public int wareHouseMaxOil;
+
+    [HideInInspector]
+    public int plasticStored;
+    [HideInInspector]
+    public int oilStored;
+    [HideInInspector]
+    public int woodStored;
+
+    void Start()
+    {
+        plasticVolume = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<WorldData>().plasticVolume;
+        woodVolume = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<WorldData>().woodVolume;
+        oilVolume = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<WorldData>().oilVolume;
+    }
+
+    void Update()
+    {
+        foreach(GameObject boat in GameObject.FindGameObjectsWithTag("Boat"))
+        {
+            if(boats.Contains(boat) == false)
+            {
+                boats.Add(boat);
+            }
+        }
+
+        foreach(GameObject boat in boats)
+        {
+            if(Vector3.Distance(transform.position, boat.transform.position) <= wareHousePickupRange)
+            {
+                Inventory boatInventory = boat.GetComponent<Inventory>();
+                if(boatInventory != null)
+                {
+                    while (boatInventory.oilStored >= oilVolume)
+                    {
+                        boatInventory.RemoveOil();
+                        AddOil();
+                    }
+                    while (boatInventory.woodStored >= woodVolume)
+                    {
+                        boatInventory.RemoveWood();
+                        AddWood();
+                    }
+                    while (boatInventory.plasticStored >= plasticVolume)
+                    {
+                        boatInventory.RemovePlastic();
+                        AddPlastic();
+                    }
+                }             
+            }
+        }
+    }
+
+    void AddPlastic()
+    {
+        plasticStored += plasticVolume;
+    }
+
+    void AddOil()
+    {
+        oilStored += oilVolume;
+    }
+
+    void AddWood()
+    {
+        woodStored += woodVolume;
+    }
+
+    public void RemovePlastic()
+    {
+        plasticStored -= plasticVolume;
+    }
+
+    public void RemoveOil()
+    {
+        oilStored -= oilVolume;
+    }
+    public void RemoveWood()
+    {
+        woodStored -= woodVolume;
+    }
+}
