030b33a187a8ef44a33e670a79c2090dcf5f5bd3
Pepijn Wasser <482906@student.saxion.nl>
2020-09-08 09:33:08 +0200

    upload

---
+++ Project Customer/Assets/scripts/LVLcount.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LVLcount : MonoBehaviour
+{
+    public Transform player;
+    public Text levelText;
+    void Update()
+    {
+        levelText.text = ((int)player.position.z).ToString();
+
+    }
+}
