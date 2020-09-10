af4cf8fedf31b8c31ac167a566354ceb62d883c7
LucRoosjen <luc.roosjen@gmail.com>
2020-09-10 10:49:05 +0200

    UI for ingame menus

---
+++ Project Customer/Assets/scripts/Movement/MoveToQue.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToQue : MonoBehaviour
+{
+    List<Vector3> que = new List<Vector3>();
+
+    int targetIndex = 0;
+    Vector3 target;
+
+    NavMeshAgent myAgent;
+
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();    
+    }
+
+    void Update() 
+    {
+        if (transform.GetComponent<MovementQue>() != null)
+        {
+            que = transform.GetComponent<MovementQue>().que;
+        }
+        if(que != null && que.Count != 0 && targetIndex < que.Count)
+        {
+            target = que[targetIndex];           
+        }
+
+        Move();
+    }
+
+    void Move()
+    {
+        if (que != null && que.Count != 0)
+        {
+            myAgent.SetDestination(target);
+
+            if (Vector3.Distance(transform.position, target) < 1)
+            {
+                if(targetIndex < que.Count - 1)
+                {
+                    targetIndex += 1;
+                }
+                else
+                {
+                    targetIndex = 0;
+                }
+            }
+        }       
+    }
+}
---
+++ Project Customer/Assets/scripts/selecting/Selected.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selected : MonoBehaviour
+{
+    [HideInInspector]
+    public bool selected = false;
+    public Material selectedMaterial;
+    public Material notSelectedMaterial;
+
+    MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = notSelectedMaterial;
+    }
+
+    void Update()
+    {
+        if(selected == true)
+        {
+            meshRenderer.material = selectedMaterial;
+        }
+        else
+        {
+            meshRenderer.material = notSelectedMaterial;
+        }
+    }
+}
