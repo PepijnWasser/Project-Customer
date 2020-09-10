23db609fef21cf8a472b40480d81fef553029c64
Pepijn Wasser <482906@student.saxion.nl>
2020-09-10 13:18:03 +0200

    prevent bugs

--- Project Customer/Assets/scripts/managers/worldData.cs
+++ Project Customer/Assets/scripts/managers/worldData.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class WorldData : MonoBehaviour
 {
     [Min(1)]
-    public int woodVolume;
+    public int woodVolume = 1;
     [Min(1)]
-    public int plasticVolume;
+    public int plasticVolume = 1;
     [Min(1)]
-    public int oilVolume;
+    public int oilVolume = 1;
     [Min(10)]
-    public int mapSize;
+    public int mapSize = 10;
 }
+
+
