178f0f7c5444a8e5268ebafb5d4c0df5dbc7fd18
Pepijn Wasser <482906@student.saxion.nl>
2020-09-14 16:52:59 +0200

    Revert "delete proj"
    This reverts commit df88268a09a546d4e248ab7119f5f055156a0f40.

---
+++ Project Customer/Assets/scripts/Tutorial/TutorialMessage.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.Networking.PlayerConnection;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TutorialMessage : MonoBehaviour
+{
+    int messageNumber = 0;
+    Text messageDisplay;
+    public string[] messages;
+    bool needToAddMovement = true;
+    bool paused;
+    bool needToCheckPlayerMoney = true;
+
+    public GameObject previous;
+    public GameObject ok;
+
+    private void Start()
+    {
+        messageDisplay = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (messages != null && messages.Count() != 0)
+        {
+            if (messageNumber <= messages.Count() - 1)
+            {
+                messageDisplay.text = messages[messageNumber];
+            }          
+        }
+
+        PreviousActivity();
+        OkActivity();
+
+        if (messageNumber == 15 && needToAddMovement == true)
+        {
+            needToAddMovement = false;
+            ZoomCamera camZoom = GameObject.Find("tilted").AddComponent<ZoomCamera>();
+            camZoom.zoomSpeed = 5;
+            camZoom.minZoom = 60;
+            camZoom.maxZoom = 10;
+
+            CameraMovement camMovement = GameObject.FindGameObjectWithTag("Camera Pivot").AddComponent<CameraMovement>();
+            camMovement.moveSpeed = 5;
+            camMovement.boostSpeed = 20;
+            camMovement.rotationSpeed = 90;
+
+            ChangeCamera camChange = GameObject.FindGameObjectWithTag("Camera Pivot").AddComponent<ChangeCamera>();
+        }
+
+        if((messageNumber == 17 || messageNumber == 18) && needToCheckPlayerMoney)
+        {
+            if(GameObject.FindGameObjectWithTag("LevelManager").GetComponent<PlayerInfo>().money == 2)
+            {
+                needToCheckPlayerMoney = false;
+                messageNumber = 19;
+            }
+        }
+    }
+       
+    void PreviousActivity()
+    {
+        if (messageNumber == 0 || messageNumber == 17 || messageNumber == 19)
+        {
+            previous.SetActive(false);
+        }
+        else
+        {
+            previous.SetActive(true);
+        }
+    }
+
+    void OkActivity()
+    {
+        if (messageNumber == 16 || messageNumber == 18)
+        {
+            ok.SetActive(false);
+        }
+        else
+        {
+            ok.SetActive(true);
+        }
+    }
+
+    public void NextMessage()
+    {
+        messageNumber += 1;
+        if(messageNumber == 21)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    public void PreviousMessage()
+    {
+            messageNumber -= 1;
+    }
+}
