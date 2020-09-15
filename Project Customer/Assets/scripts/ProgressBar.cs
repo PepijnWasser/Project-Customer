1c864c16949915e31ac359b0dd681a71262dd5ba
LucRoosjen <luc.roosjen@gmail.com>
2020-09-15 16:15:11 +0200

    vol

---
+++ Project Customer/Assets/scripts/ProgressBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    public int minimum;
+    public int maximum;
+    int current;
+
+    public Image mask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+    void GetCurrentFill()
+    {
+        current = GameObject.FindGameObjectWithTag("LevelManager").GetComponent<PlayerInfo>().fuel;
+        float currentOffset = current - minimum; // current - minimum
+        float maximumOffset = maximum - minimum;
+        float fillAmount = currentOffset / maximumOffset;
+        mask.fillAmount = fillAmount;
+    }
+}
---
+++ Project Customer/Assets/scripts/spawning/spawnAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnAnimation : MonoBehaviour
+{
+    [Min(0)]
+    public float spawnDuration;
+
+    Vector3 originalScale;
+
+    float timer = 0;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+        transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        if (timer < spawnDuration)
+        {
+            timer += Time.deltaTime;
+            transform.localScale = originalScale * (timer / spawnDuration);
+        }
+    }
+}
