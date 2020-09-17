90ee1223f7ded20b7eaba56ca4f20232d7d3783b
Pepijn Wasser <482906@student.saxion.nl>
2020-09-17 13:18:24 +0200

    changed using

--- Project Customer/Assets/scripts/Tutorial/TutorialMessage.cs
+++ Project Customer/Assets/scripts/Tutorial/TutorialMessage.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.VersionControl;
+﻿using System.Linq;
 using UnityEngine;
-using UnityEngine.Networking.PlayerConnection;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
